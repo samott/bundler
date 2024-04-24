a7cbc9542910fe1ea9513eee036cb1853171d8d0
Shaun Amott <shaun@inerd.com>
2024-04-24 22:14:39 +0200

    Create a narrower interface for the client
    TypeScript was having trouble resolving the previous type, and
    there was a bunch of stuff we technically didn't need in there.

--- src/blockchain.service.ts
+++ src/blockchain.service.ts
@@ -12,14 +12,64 @@ import erc4337Abi from './abis/erc4337.json';
 
 import { UserOperationDto } from './dto/user-operation.dto';
 
-type CombinedClient =
-	ReturnType<typeof createPublicClient>
-		& ReturnType<typeof createWalletClient>;
+import {
+	ReadContractParameters,
+	ReadContractReturnType,
+	WriteContractParameters,
+	WriteContractReturnType,
+	WaitForTransactionReceiptParameters,
+	WaitForTransactionReceiptReturnType,
+	Chain,
+	Abi,
+	ContractFunctionName,
+	ContractFunctionArgs,
+} from 'viem';
+
+type IViemClient = {
+	readContract<
+		chain extends Chain | undefined,
+		const abi extends Abi | readonly unknown[],
+		functionName extends ContractFunctionName<abi, 'pure' | 'view'>,
+		const args extends ContractFunctionArgs<abi, 'pure' | 'view', functionName>,
+	>(
+		parameters: ReadContractParameters<abi, functionName, args>,
+	) : Promise<ReadContractReturnType<abi, functionName, args>>;
+
+	writeContract<
+		chain extends Chain | undefined,
+		account extends Account | undefined,
+		const abi extends Abi | readonly unknown[],
+		functionName extends ContractFunctionName<abi, 'nonpayable' | 'payable'>,
+		args extends ContractFunctionArgs<
+			abi,
+			'nonpayable' | 'payable',
+			functionName
+		>,
+		chainOverride extends Chain | undefined,
+	>(
+		parameters: WriteContractParameters<
+			abi,
+			functionName,
+			args,
+			chain,
+			account,
+			chainOverride
+		>,
+	) : Promise<WriteContractReturnType>;
+
+	waitForTransactionReceipt<
+		TChain extends Chain | undefined,
+	>(
+		params: WaitForTransactionReceiptParameters<TChain>
+	) : Promise<WaitForTransactionReceiptReturnType<TChain>>
+
+	chain: Chain;
+};
 
 @Injectable()
 export class BlockchainService {
 	constructor(
-		readonly client: CombinedClient,
+		readonly client: IViemClient,
 		readonly account: Account,
 	) {};
 
