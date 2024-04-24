9ce185eb8f307ec542c236310402e712d5cd6ff7
Shaun Amott <shaun@inerd.com>
2024-04-24 19:06:39 +0200

    Add DTO for UserOperation and jsonrpc wrappers.

---
+++ src/dto/user-operation.dto.ts
@@ -0,0 +1,74 @@
+import {
+	IsInt,
+	IsNumberString,
+	IsHexadecimal,
+	IsEthereumAddress,
+	IsString,
+	IsArray,
+	Equals,
+	ValidateNested,
+} from 'class-validator';
+
+import { Type } from 'class-transformer';
+
+import { IsHexData } from '../decorators/is-hex-data.decorator';
+
+export class UserOperationDto {
+	@IsEthereumAddress()
+	sender: string;
+
+	@IsHexData({ disallowEmpty: true })
+	nonce: string;
+
+	@IsHexData()
+	initCode: string;
+
+	@IsHexData()
+	callData: string;
+
+	@IsHexData()
+	callGasLimit: string;
+
+	@IsHexData()
+	verificationGasLimit: string;
+
+	@IsHexData()
+	preVerificationGasLimit: string;
+
+	@IsHexData()
+	preVerificationGas: string;
+
+	@IsHexData()
+	maxFeePerGas: string;
+
+	@IsHexData()
+	maxPriorityFeePerGas: string;
+
+	@IsHexData({ disallowEmpty: true })
+	signature: string;
+}
+
+export class JsonRpcUserOperationDto {
+	@IsString()
+	@Equals('2.0')
+	jsonrpc: string;
+
+	@IsString()
+	@Equals('eth_sendUserOperation')
+	method: string;
+
+	@IsArray()
+	@ValidateNested({ each: true })
+	@Type(() => UserOperationDto)
+	params: UserOperationDto[];
+}
+
+export class JsonRpcResponseDto {
+	@IsString()
+	@Equals('2.0')
+	jsonrpc: string;
+
+	@IsString()
+	@Equals('eth_sendUserOperation')
+	method: string;
+}
