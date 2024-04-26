d6aa0f8e297368de063b0378dca421b78ed19a33
Shaun Amott <shaun@inerd.com>
2024-04-26 22:20:03 +0200

    Move e2e tests into existing test directory.

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,85 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { Test } from '@nestjs/testing';
+import { ConfigService } from '@nestjs/config';
+import { INestApplication } from '@nestjs/common';
 
-describe('AppController (e2e)', () => {
+import { ValidationPipe } from '@nestjs/common';
+import { AppModule } from '../src/app.module';
+
+import { createUserOp } from '../src/test/utils';
+
+describe('App', () => {
 	let app: INestApplication;
+	let configService: ConfigService;
 
-	beforeEach(async () => {
-		const moduleFixture: TestingModule = await Test.createTestingModule({
+	beforeAll(async () => {
+		const module = await Test.createTestingModule({
 			imports: [AppModule],
 		}).compile();
 
-		app = moduleFixture.createNestApplication();
+		app = module.createNestApplication();
+
+		app.useGlobalPipes(
+			new ValidationPipe({
+				whitelist: true,
+				transform: true,
+			})
+		);
+
 		await app.init();
+
+		configService = module.get<ConfigService>(ConfigService);
 	});
 
-	it('/ (GET)', () => {
-		return request(app.getHttpServer())
-			.get('/')
-			.expect(200)
-			.expect('Hello World!');
+	it('Should successfully post a user op', async () => {
+		const userOp = await createUserOp(configService);
+
+		const res = await request(app.getHttpServer())
+			.post('/rpc')
+			.send({
+				jsonrpc: '2.0',
+				id: 42,
+				method: 'eth_sendUserOperation',
+				params: [
+					userOp
+				]
+			});
+
+		expect(res.body).toMatchObject(expect.objectContaining({
+			jsonrpc: '2.0',
+			method: 'eth_sendUserOperation',
+			id: 42,
+			result: expect.objectContaining({
+				status: 'submitted',
+				txHash: expect.stringMatching(/^0x[a-fA-F0-9]{1,}/)
+			})
+		}));
+	});
+
+	it('Should fail due to invalid parameters', async () => {
+		const userOp = { sender: "z" };
+
+		const res = await request(app.getHttpServer())
+			.post('/rpc')
+			.send({
+				jsonrpc: '2.0',
+				id: 42,
+				method: 'eth_sendUserOperation',
+				params: [
+					userOp
+				]
+			});
+
+		expect(res.body).toMatchObject(expect.objectContaining({
+			id: 42,
+			jsonrpc: '2.0',
+			error: expect.objectContaining({
+				code: -32600
+			})
+		}));
+	});
+
+	afterAll(async () => {
+		await app.close();
 	});
 });
